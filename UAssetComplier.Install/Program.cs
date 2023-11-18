f9585a79a06212365ee46998ed50c380e2261619
iris <1129737123@qq.com>
2023-11-18 20:44:21 +0800

    update UAssetComplier.Install

--- UAssetComplier.Install/Program.cs
+++ UAssetComplier.Install/Program.cs
@@ -1,15 +1,23 @@
-﻿
-
-var curPath = Environment.CurrentDirectory;
+﻿var curPath = Environment.CurrentDirectory;
 var pathStr = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
+
+Console.WriteLine("Starting set System Path...");
 
 if (!pathStr!.Contains(curPath))
 {
     Environment.SetEnvironmentVariable("Path", pathStr + ";" + curPath, EnvironmentVariableTarget.Machine);
     Console.WriteLine("Path set success！");
 }
-else {
+else
+{
     Console.WriteLine("Path already set！");
+}
+
+Console.WriteLine("Do you need to add it to the system's context menu? Y/N");
+var result = Console.ReadLine();
+if (result! == "Y" || result! == "y")
+{
+    Console.WriteLine("Set system's context menu success!");
 }
 
 Console.WriteLine("Press any key to exit...");
--- UAssetComplier.Install/RegistryHelper.cs
+++ UAssetComplier.Install/RegistryHelper.cs
@@ -5,20 +5,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UAssetComplier.Install
+namespace UAssetCompiler.Install
 {
     public class RegistryHelper
     {
         /// <summary>
         /// 计算机\HKEY_CURRENT_USER\Software\Classes\*\shell
         /// </summary>
-        private static RegistryKey RegistryUserSoftwareClassesAllShell
-        {
-            get
-            {
-                return Registry.CurrentUser.OpenSubKey("Software").OpenSubKey("Classes").OpenSubKey("*").OpenSubKey("shell", true);
-            }
-        }
+        private static RegistryKey? RegistryUserSoftwareClassesAllShell =>
+            Registry.CurrentUser
+                .OpenSubKey("Software")!
+                .OpenSubKey("Classes")!
+                .OpenSubKey("*")!
+                .OpenSubKey("shell", true);
+
         /// <summary>
         /// 生成右键菜单按钮（当前用户）
         /// </summary>
@@ -26,7 +26,7 @@ namespace UAssetComplier.Install
         /// <param name="programPath">点击按钮后打开的程序路径</param>
         public static void CreateUserContextMenu(string keyName, string programPath)
         {
-            if (!RegistryUserSoftwareClassesAllShell.GetSubKeyNames().Contains(keyName))
+            if (!RegistryUserSoftwareClassesAllShell!.GetSubKeyNames().Contains(keyName))
             {
                 var contextMenuRegistryKey = RegistryUserSoftwareClassesAllShell.CreateSubKey(keyName);
                 contextMenuRegistryKey.SetValue("", keyName);
@@ -40,7 +40,7 @@ namespace UAssetComplier.Install
         /// <param name="keyName">右键菜单名</param>
         public static void DeleteUserContextMenu(string keyName)
         {
-            if (RegistryUserSoftwareClassesAllShell.GetSubKeyNames().Contains(keyName))
+            if (RegistryUserSoftwareClassesAllShell!.GetSubKeyNames().Contains(keyName))
             {
                 RegistryUserSoftwareClassesAllShell.DeleteSubKeyTree(keyName);
             }
